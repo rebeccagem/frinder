9ed81c2a40efad60b8689dd58c6346c3f1278bca
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-04-25 22:16:41 -0500

    css working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,27 @@
+//gets array from friends.js
+var friends = require("../data/friends");
 //A GET Route to /survey which should display the survey page.
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/survey", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "survey.html"));
-  });
+// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+// Displays all characters
+module.exports = function(app){
+app.get("/api/friends", function (req, res) {
+  return res.json(friends);
+});
 
-// A default, catch-all route that leads to home.html which displays the home page.
+//   A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+// Create New Characters - takes in JSON input
+app.post("/api/friends", function (req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newFriend = req.body;
 
+  console.log(newFriend);
 
+  // We then add the json the user sent to the character array
+  friends.push(newFriend);
+
+  // We then display the JSON to the users
+  res.json(newFriend);
+});
+}
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+
+// A default, catch-all route that leads to home.html which displays the home page.
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+  
+  // A default, catch-all route that leads to home.html which displays the home page.
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  }
