d79669de0597a2694fe8355c4a6290ab2af79575
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-04-28 09:56:03 -0500

    heroku fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.envPORT ||3000;
+var PORT = process.env.PORT ||3000;
 
 //for css
 console.log(path.join(__dirname+ '/public'));
